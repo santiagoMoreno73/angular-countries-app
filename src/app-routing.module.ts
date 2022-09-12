e646b40a495f27cc73323e650232ded45810566f
santiagoMoreno73 <santimor2009@hotmail.com>
2022-09-12 10:41:00 -0500

    [add]: routing app

---
+++ src/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { ByCapitalComponent } from './app/country/pages/by-capital/by-capital.component';
+import { ByCountryComponent } from './app/country/pages/by-country/by-country.component';
+import { ByRegionComponent } from './app/country/pages/by-region/by-region.component';
+import { SeeCountryComponent } from './app/country/pages/see-country/see-country.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ByCountryComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'region',
+    component: ByRegionComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'capital',
+    component: ByCapitalComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'country/:id_country',
+    component: SeeCountryComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [],
+  exports: [],
+
+  providers: [],
+})
+export class NameModule {}
