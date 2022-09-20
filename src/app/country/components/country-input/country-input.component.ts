c91954fa03d1b483c315f29c7e96916953cbe1f4
santiagoMoreno73 <santimor2009@hotmail.com>
2022-09-20 12:43:08 -0500

    [add]: input component

---
+++ src/app/country/components/country-input/country-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-country-input',
+  templateUrl: './country-input.component.html',
+  styles: [],
+})
+export class CountryInputComponent implements OnInit {
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();
+  term: string = '';
+
+  constructor() {}
+
+  ngOnInit() {
+    this.debouncer.pipe(debounceTime(300)).subscribe((value) => {
+      this.onDebounce.emit(value);
+    });
+  }
+
+  search() {
+    this.onEnter.emit(this.term);
+  }
+
+  onKeyPress() {
+    this.debouncer.next(this.term);
+  }
+}
