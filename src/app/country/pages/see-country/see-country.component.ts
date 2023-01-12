5732a405bdcab61bd8328b73a05cac192c16f4e3
santiagoMoreno73 <santimor2009@hotmail.com>
2023-01-12 16:53:28 -0500

    add component see-country

--- src/app/country/pages/see-country/see-country.component.ts
+++ src/app/country/pages/see-country/see-country.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 @Component({
   selector: 'app-see-country',
   templateUrl: './see-country.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SeeCountryComponent implements OnInit {
+  country!: Country;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private serviceCountry: CountryService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id_country }) =>
+          this.serviceCountry.getCountryAlpha(id_country)
+        ),
+        tap(console.log)
+      )
+      .subscribe((country) => {
+        this.country = country;
+      });
 
+    // this.activatedRoute.params.subscribe(({ id_country }) => {
+    //   this.serviceCountry.getCountryAlpha(id_country).subscribe((country) => {
+    //     this.country = country;
+    //     console.log("here", this.country);
+    //   });
+    // });
+  }
 }
