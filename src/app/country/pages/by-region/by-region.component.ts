f00627f3001559d82b91681cbe924a21998a0578
santiagoMoreno73 <santimor2009@hotmail.com>
2023-01-12 16:53:17 -0500

    add changes by region

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -1,16 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
+  regions: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC',
+  ];
+  regionActive: string = '';
+  countriesRegion: Country[] = [];
 
-  constructor() { }
+  constructor(private appService: CountryService) {}
 
-  ngOnInit(): void {
+  activatedRegion(cod_region: string) {
+    if (this.regionActive == cod_region) {
+      return;
+    }
+
+    this.regionActive = cod_region;
+    this.countriesRegion = [];
+
+    this.appService
+      .searchRegion(cod_region)
+      .subscribe((countries) => this.handleResult(countries));
   }
 
+  getClassScss(region: string) {
+    return region === this.regionActive
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
+
+  handleResult(countries: Country[]) {
+    console.log('Region', countries);
+    this.countriesRegion = countries;
+  }
 }
