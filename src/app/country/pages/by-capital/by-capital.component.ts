29592a0987b394bd4dabbcf752ab8fa5edac411f
santiagoMoreno73 <santimor2009@hotmail.com>
2023-01-12 16:53:03 -0500

    add changes by capital

--- src/app/country/pages/by-capital/by-capital.component.ts
+++ src/app/country/pages/by-capital/by-capital.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital',
   templateUrl: './by-capital.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCapitalComponent implements OnInit {
+  term: string = '';
+  isError: boolean = false;
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private appService: CountryService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  search(event: string) {
+    this.isError = false;
+    this.term = event;
+    this.appService.searchCapital(this.term).subscribe(
+      (countries) => {
+        this.countries = countries;
+      },
+      (error) => {
+        this.isError = true;
+        this.countries = [];
+      }
+    );
   }
 
+  suggestions(term: string) {
+    this.isError = false;
+  }
 }
