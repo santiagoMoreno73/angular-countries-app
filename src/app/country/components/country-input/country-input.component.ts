41c03dcab10f410527d479b8a1ad285cce2faba9
santiagoMoreno73 <santimor2009@hotmail.com>
2023-01-12 16:52:34 -0500

    country input changes

--- src/app/country/components/country-input/country-input.component.ts
+++ src/app/country/components/country-input/country-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
@@ -10,6 +10,7 @@ import { debounceTime } from 'rxjs/operators';
 export class CountryInputComponent implements OnInit {
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
   @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+  @Input() placeHolderTxt: string = ''; 
 
   debouncer: Subject<string> = new Subject();
   term: string = '';
